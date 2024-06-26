b1df6c10872e5b1b36b28bd5d1a7b29bdd951e56
git-cameronbryce <developmentcameronbryce@gmail.com>
2024-06-26 11:09:33 -0600

    feat: Add 'nga-gameserver-stop' slash command
    - Add a command to allow administrators to stop game servers.
    - Command requires an identifier to target specific instances.

---
+++ source/command-handler/server-commands/ng-server-restart.ts
@@ -0,0 +1,17 @@
+import { SlashCommandBuilder } from 'discord.js';
+import type { SlashCommandProps, CommandOptions } from 'commandkit';
+import axios, { AxiosResponse } from 'axios';
+
+export const data = new SlashCommandBuilder()
+  .setName('nga-gameserver-restart')
+  .setDescription('Performs an in-game server action.')
+  .addStringOption(option => option.setName('identifier').setDescription('Selected action will be performed on given server.').setRequired(true))
+
+export async function run({ interaction, client, handler }: SlashCommandProps) {
+  await interaction.deferReply({ ephemeral: false });
+
+};
+
+export const options: CommandOptions = {
+  userPermissions: ['Administrator'],
+};
--- source/command-handler/server-commands/ng-server-stop.ts
+++ source/command-handler/server-commands/ng-server-stop.ts
@@ -3,13 +3,12 @@ import type { SlashCommandProps, CommandOptions } from 'commandkit';
 import axios, { AxiosResponse } from 'axios';
 
 export const data = new SlashCommandBuilder()
-  .setName('gameserver-restart')
+  .setName('nga-gameserver-stop')
   .setDescription('Performs an in-game server action.')
   .addStringOption(option => option.setName('identifier').setDescription('Selected action will be performed on given server.').setRequired(true))
 
 export async function run({ interaction, client, handler }: SlashCommandProps) {
   await interaction.deferReply({ ephemeral: false });
-
 
 };
 
