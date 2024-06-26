a586b9002a69c14f3750536321bd7152374dc105
git-cameronbryce <developmentcameronbryce@gmail.com>
2024-06-26 11:15:29 -0600

    refactor: Edit configuration and integrate Supabase client - Reorganize configuration files into 'other-config' directory - Enable Supabase for database interactions and event handling in CommandKit

--- source/script.ts
+++ source/script.ts
@@ -1,18 +1,23 @@
-import { discord } from './other/configuration.json'
+import { discord } from './other-config/configuration.json'
 import { Client, GatewayIntentBits } from 'discord.js';
+import { createClient } from '@supabase/supabase-js'
 import { CommandKit } from 'commandkit';
+import { database } from './other-config/configuration.json'
+
+export const supabase = createClient(database.url, database.key)
 
 import path from 'path';
-const client = new Client({
+const client: Client<boolean> = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
 
 new CommandKit({
   client,
   commandsPath: path.join(__dirname, 'command-handler'),
-  // eventsPath: path.join(__dirname, 'event-handler'),
+  eventsPath: path.join(__dirname, 'event-handler'),
   devGuildIds: ['1219480518131716126'],
   bulkRegister: true
 });
+
 
 client.login(discord.token).then(() => console.log('Client logged in...'))
